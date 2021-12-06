4ce0f32daf3aee6925602365d488067218dec7ce
Thomas Angelbo Christensen <thomas@eject.io>
2021-12-06 13:31:54 +0100

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # material-icons-extract
+
+Updates /ui with the latest Material Design "two-tone" icons.
+
+`$ git clone git@github.com:google/material-design-icons.git`
+
+`$ node index.js`
+
+Copy folder "./assets" to "/ui/src/Icon/" to update all icons.
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { resolve } = require("path");
 const { readdir } = require("fs").promises;
 const fs = require("fs").promises;
+const shell = require("shelljs");
+
+const generateCss = (id) => `.${id} { mask-image: url("./assets/${id}.svg"); }\n`;
 
 async function* getFiles(dir) {
   const dirents = await readdir(dir, { withFileTypes: true });
@@ -17,18 +20,41 @@ async function* getFiles(dir) {
 }
 
 (async () => {
-  for await (const f of getFiles("./src")) {
-    if (f.toString().lastIndexOf("twotone") !== -1) {
+  const inDir = "src";
+  const outDir = "assets";
+  const outCss = `${outDir}/all.css`;
+
+  try {
+    await fs.mkdir(outDir);
+  } catch (error) {
+    // ...
+  }
+
+  let files = [];
+  let css = "";
+
+  for await (const f of getFiles(inDir)) {
+    if (f.toString().lastIndexOf("twotone") !== -1 && f.lastIndexOf("24px") !== -1) {
       const a = f.split("/");
-      const destination = `out/${a[a.length - 3]}.svg`;
+
+      const id = `${a[a.length - 3]}`;
+      const destination = `${outDir}/${id}.svg`;
+
+      css += generateCss(id);
 
       try {
         fs.copyFile(f, destination);
-
-        console.log(destination);
+        console.info(`copied ${destination}`);
       } catch (error) {
         throw error;
       }
     }
+  }
+
+  try {
+    fs.writeFile(outCss, css);
+    console.info(`generated ${outCss}`);
+  } catch (error) {
+    throw error;
   }
 })();
